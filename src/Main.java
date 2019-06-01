5247e30fe1d40d51bdb9882f7889d0b4adfa7613
dhoxie <dhoxie@eagles.ewu.edu>
2019-05-31 17:32:43 -0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,46 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) {
+        BST bst = new BST();
         Scanner in = new Scanner(System.in);
-        BinarySearchTree tree = new BinarySearchTree();
+        bst.console(in);
+        in.close();
+    }
+}
+
+class BST{
+
+    public void console(Scanner in){
         boolean done = false;
         while (!done && in.hasNextLine()){
             String line = in.nextLine();
             Scanner ls = new Scanner(line);
             String token = ls.next();
+            Node node;
             switch (token.toUpperCase()){
                 case "INSERT":
                     while (ls.hasNext()){
-                        tree.insert(ls.nextInt(), ls.next().charAt(0));
+                        node = new Node(ls.nextInt(), ls.next().charAt(0));
+                        insert(node);
                     }
                     break;
                 case "DELETE":
-                    while (ls.hasNextInt()){
-                        tree.delete(ls.nextInt());
+                    while (ls.hasNext()){
+                        int key = ls.nextInt();
+                        Node z = treeSearch(root, key);
+                        if (z != nil) delete(z);
                     }
                     break;
                 case "PREORDER":
-                    tree.preorder();
+                    preorder();
                     break;
                 case "INORDER":
-                    tree.inorder();
+                    inorder();
                     break;
                 case "POSTORDER":
-                    tree.postorder();
+                    postorder();
                     break;
                 case "EXIT":
                     ls.close();
@@ -36,115 +48,116 @@ class Main {
                     break;
             }
         }
-        in.close();
     }
-}
 
-class BinarySearchTree{
-    class Node{
-        private char data;
-        private int key;
-        Node left, right, p;
+    private class Node{
+        char data;
+        int key;
+        Node l = nil, r = nil, p = nil;
 
-        Node(int k, char d){
-            key = k;
-            data = d;
+        Node(int key){ this.key = key;}
+        Node(int key, char data){
+            this.key = key;
+            this.data = data;
+        }
+    }//end Node class
+
+    private final Node nil = new Node(-1);
+    private Node root = nil;
+
+    private void insert(Node z) {
+        Node y = nil;
+        Node x = root;
+        while (x != nil){
+            y = x;
+            if (z.key < x.key) x = x.l;
+            else x = x.r;
+        }
+        z.p = y;
+        if (y == nil) root = z;
+        else if (z.key < y.key) y.l = z;
+        else y.r = z;
+    }
+
+    private void delete(Node z){
+        if (z.l == nil) transplant(z, z.r);
+        else if (z.r == nil) transplant(z, z.l);
+        else{
+            Node y = min(z.r);
+            if (y.p != z){
+                transplant(y, y.r);
+                y.r = z.r;
+                y.r.p = y;
+            }
+            transplant(z, y);
+            y.l = z.l;
+            y.l.p = y;
         }
     }
 
-    private Node root;
-
-    void insert(int key, char data){
-        this.root = insert(root, key, data);
-    }
-
-    private Node insert(Node root, int key, char data){
-        if (root == null) return new Node(key, data);
-        if (root.key - key >= 0)
-            root.left = insert(root.left, key, data);
-        else
-            root.right = insert(root.right, key, data);
-        return root;
-    }
-
-    void delete(int key){
-        root = delete(root, key);
-    }
-
-    private Node delete(Node rt, int key){
-        if (rt == null) return null;
-        if (rt.key > key)
-            rt.left = delete(rt.left, key);
-        else if (rt.key < key)
-            rt.right = delete(rt.right, key);
-        else{ //found node to remove
-            if (rt.left == null) return rt.right;
-            else if (rt.right == null) return rt.left;
-            //two children
-            rt.key = minValue(rt.right);
-            rt.right = delete(rt.right, rt.key);
-            //else {
-            //    Node temp = getMax(rt.left);
-            //    rt = temp;
-            //    rt.left = deleteMax(rt.left);
-            //}
+    private Node successor(Node x){
+        if (x.r != nil) return min(x.r);
+        Node y = x.p;
+        while (y != nil && x == y.r){
+            x = y;
+            y = y.p;
         }
-        return rt;
+        return y;
     }
 
-    private int minValue(Node rt){
-        while (rt.left != null){
-            rt = rt.left;
-        }
-        return rt.key;
+    private Node min(Node x){
+        while (x.l != nil)
+            x = x.l;
+        return x;
     }
 
-    private Node getMax(Node rt){
-        if (rt.right == null) return rt;
-        return getMax(rt.right);
+    private void transplant(Node u, Node v){
+        if (u.p == nil) root = v;
+        else if (u == u.p.l) u.p.l = v;
+        else u.p.r = v;
+        if (v != nil) v.p = u.p;
     }
 
-    private Node deleteMax(Node rt){
-        if (rt.right == null) {
-            if (rt.left != null) return rt.left;
-        }
-        else rt.right = deleteMax(rt.right);
-        return rt;
+    private Node treeSearch(Node x, int key){
+        if (x == nil || key == x.key) return x;
+        if (key < x.key) return treeSearch(x.l, key);
+        return treeSearch(x.r, key);
     }
 
-    void inorder(){
+    private void inorder(){
         inorder(root);
         System.out.println();
     }
 
-    private void inorder(Node root){
-        if (root == null) return;
-        inorder(root.left);
-        System.out.print(root.data);
-        inorder(root.right);
+    private void inorder(Node rt){
+        if (rt == nil) return;
+        inorder(rt.l);
+        System.out.print(rt.data);
+        inorder(rt.r);
     }
 
-    void postorder(){
+    private void postorder(){
         postorder(root);
         System.out.println();
     }
 
-    private void postorder(Node root){
-        if (root == null) return;
-        postorder(root.left);
-        postorder(root.right);
-        System.out.print(root.data);
+    private void postorder(Node rt){
+        if (rt == nil) return;
+        postorder(rt.l);
+        postorder(rt.r);
+        System.out.print(rt.data);
     }
 
-    void preorder(){
+    private void preorder(){
         preorder(root);
         System.out.println();
     }
 
-    private void preorder(Node root){
-        if (root == null) return;
-        System.out.print(root.data);
-        preorder(root.left);
-        preorder(root.right);
+    private void preorder(Node rt){
+        if (rt == nil) return;
+        System.out.print(rt.data);
+        preorder(rt.l);
+        preorder(rt.r);
     }
+
 }
